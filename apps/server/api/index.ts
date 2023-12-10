a068a9b034525c9ef2e332bfe5231768b50c80bd
Xuan Phu <xuanphu1024@gmailcom>
2023-12-10 13:48:36 +0700

    feat(master): add api sign up

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -4,5 +4,6 @@ import { handle } from '@hono/node-server/vercel';
 const app = new Hono().basePath('/api');
 
 app.get('/sign-in', (c) => c.json({ token: '123321' }))
+app.get('/sign-up', (c) => c.json({ token: '123321' }));
 
 export default handle(app);
