4d9dc22d6e0f708568a2edd4062ba64d78371b02
Xuan Phu <xuanphu1024@gmailcom>
2023-12-17 02:00:19 +0700

    test

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -12,7 +12,7 @@ app.use('*', logger());
 app.use(
   '*',
   cors({
-    origin: ['http://localhost:5173', 'https://discord-web-clone.vercel.app'],
+    origin: ['http://localhost:5173', 'https://discord-api-clone.vercel.app/'],
     credentials: true,
     allowHeaders: ['Content-Type', 'Authorization'],
   })
