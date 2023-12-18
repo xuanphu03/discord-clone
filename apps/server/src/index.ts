ae2b41be40972242d3de5ce6dcb9016b5ae153cd
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 01:17:12 +0700

    test

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,13 @@
 import { Hono } from 'hono';
-import { serve } from '@hono/node-server';
+import { handle } from 'hono/vercel';
 
-const bootstrap = async () => {
-  const app = new Hono().basePath('/api');
-
-  app.get('/', (c) => c.json({ message: 'Hello world!' }));
-  app.get('/sign-in', (c) => c.json({ token: '123321' }));
-
-  serve(app);
-  console.log('Listening on http://localhost:3000/api');
+export const config = {
+  runtime: 'edge',
 };
 
-bootstrap();
+const app = new Hono().basePath('/api');
+
+app.get('/', (c) => c.json({ message: 'Hello world!' }));
+app.get('/sign-in', (c) => c.json({ token: '123321' }));
+
+export default handle(app);
