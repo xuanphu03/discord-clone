cd2eef87763146dd7abef0ab40df04422825fa11
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 02:00:41 +0700

    feat(master): add api

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,5 +8,6 @@ export const config = {
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => c.json({ message: 'HELLO WORLD!!!' }))
+app.post('/sign-in', (c) => c.json({ token: '123321' }));
 
 export default handle(app)
