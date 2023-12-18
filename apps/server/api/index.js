9e3893cf430b782a6175d575d77effda028d06d7
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 04:32:36 +0700

    test

--- apps/server/api/index.js
+++ apps/server/api/index.js
@@ -1,1 +1,1 @@
-export { default } from '../public/index.js';
+export { default } from '../src/index.ts';
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,7 +4,7 @@ import { handle } from 'hono/vercel';
 export const runtime = 'edge';
 
 const app = new Hono();
-
+//hihi
 app.get('/', (c) => c.text("Hello"));
 app.get('/sign-in', (c) =>  c.json({ token: '123312' }));
 
