ed012ed77da3eab57fdf43235f2ff9cd4cabe66d
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 01:29:39 +0700

    test

--- apps/server/public/index.d.ts
+++ apps/server/public/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+export declare const config: {
+    runtime: string;
+};
+declare const _default: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
+export default _default;
--- apps/server/public/index.js
+++ apps/server/public/index.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
 const hono_1 = require("hono");
-const node_server_1 = require("@hono/node-server");
-const bootstrap = async () => {
-    const app = new hono_1.Hono().basePath('/api');
-    app.get('/', (c) => c.json({ message: 'Hello world!' }));
-    app.get('/sign-in', (c) => c.json({ token: '123321' }));
-    (0, node_server_1.serve)(app);
-    console.log('Listening on http://localhost:3000/api');
+const vercel_1 = require("hono/vercel");
+exports.config = {
+    runtime: 'edge',
 };
-bootstrap();
+const app = new hono_1.Hono().basePath('/api');
+app.get('/', (c) => c.json({ message: 'Hello world!' }));
+app.get('/sign-in', (c) => c.json({ token: '123321' }));
+exports.default = (0, vercel_1.handle)(app);
 //# sourceMappingURL=index.js.map
