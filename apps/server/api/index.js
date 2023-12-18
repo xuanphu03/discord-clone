bc9e56ea11c2094f2d32dfe56c3352a20a228bb6
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 04:03:28 +0700

    test

--- apps/server/api/index.js
+++ apps/server/api/index.js
@@ -1,1 +1,15 @@
-export { default } from '../public/index.js';
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const bootrap = async () => {
+  const app = new Hono().basePath('/api');
+
+  app.get('/', (c) => c.json({ message: 'Hello world!!!' }));
+  app.get('/sign-in', (c) => c.json({ token: '123321' }));
+  app.get('/sign-up', (c) => c.json({ token: '123312321' }));
+
+  serve(app);
+  console.log('http://localhost:3000');
+};
+
+bootrap();
