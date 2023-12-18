1873e6735e1429a7942da30310d2f3e51f00d3a8
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 01:02:42 +0700

    test

--- apps/server/public/index.js
+++ apps/server/public/index.js
@@ -4,7 +4,8 @@ const hono_1 = require("hono");
 const node_server_1 = require("@hono/node-server");
 const bootstrap = async () => {
     const app = new hono_1.Hono().basePath('/api');
-    app.get('/', (c) => c.json({ message: 'Hello world! hhdkjsfhdi' }));
+    app.get('/', (c) => c.json({ message: 'Hello world!' }));
+    app.get('/sign-in', (c) => c.json({ token: '123321' }));
     (0, node_server_1.serve)(app);
     console.log('Listening on http://localhost:3000/api');
 };
