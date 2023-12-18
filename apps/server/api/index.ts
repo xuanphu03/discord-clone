998095cfb995e7c10e8f740adfa00428d16c34bc
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 03:00:16 +0700

    test

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -10,6 +10,5 @@ const app = new Hono().basePath('/api');
 app.post('/', (c) => c.json({ message: 'HELLO WORLD!!!' }));
 app.post('/sign-in', (c) => c.json({ token: '123321' }));
 app.post('/sign-up', (c) => c.json({ token: '123abc321' }));
-console.log('123');
 
 export default handle(app)
