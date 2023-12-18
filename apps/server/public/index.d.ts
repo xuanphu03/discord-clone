2bc08c14fd55c4d9508640dbf0b5ce7513d2fe34
Xuan Phu <xuanphu1024@gmailcom>
2023-12-19 01:40:49 +0700

    feat(master): delete api

--- apps/server/public/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const config: {
-    runtime: string;
-};
-declare const _default: (req: Request, requestContext: import("hono/types").FetchEventLike) => Response | Promise<Response>;
-export default _default;
