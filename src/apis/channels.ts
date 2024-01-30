f791a5542e40324542034d8b37bd014fc46ee6b2
Xuan Phu <xuanphu1024@gmail.com>
2024-01-30 09:07:21 +0700

    feat(master): add api get members of org

--- src/apis/channels.ts
+++ src/apis/channels.ts
@@ -8,3 +8,17 @@ interface Channel {
 export const getChannels = async (orgID: string) => {
   return request.get<Channel[]>(`orgs/${orgID}/channels`)
 }
+
+interface Member {
+  displayName: string;
+  username: string;
+  avatar: string;
+  memberSince: string;
+  joinedDiscord: string;
+  joinMethod: string;
+  roles: string[];
+}
+
+export const getChannelMembers = async (orgID: string, channelID: string) => {
+  return request.get<Member[]>(`/orgs/${orgID}/channels/${channelID}/members`)
+}
